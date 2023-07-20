700557f7703dd0151771d5e9f786e66f57e6137c
jerry8534 <jerryjjinu@gmail.com>
2023-07-20 23:09:39 +0900

    20.07.23

---
+++ leetcode/java/Palindrome Number.java
@@ -0,0 +1,16 @@
+package leetcode.java;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int temp = x;
+        int reverse = 0;
+        while(temp != 0){
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return reverse == x;
+    }
+}
