0dd375a4a038b946641503c210f0a1f08db6b847
jerry8534 <jerryjjinu@gmail.com>
2025-07-21 21:13:23 +0900

    programmers

--- programmers/DP/ExpressionInN.java
+++ programmers/DP/ExpressionInN.java
@@ -6,31 +6,25 @@ class Solution {
             return 1;
         }
 
-        // 가능한 숫자들의 집합을 담을 리스트 초기화
+        // init list of set
         List<Set<Integer>> dp = new ArrayList<>();
         for (int i = 0; i <= 8; i++) {
             dp.add(new HashSet<>());
         }
 
-        // 숫자 N을 이용하여 만들 수 있는 숫자들 초기화
+        // add N for 1
         dp.get(1).add(N);
 
-        // 숫자를 더하거나 빼는 연산을 적용하여 가능한 숫자들 생성
+        // search possible number made by N using i times
         for (int i = 2; i <= 8; i++) {
-            // 현재 i에 해당하는 숫자를 만들기 위해 N을 사용하는 경우의 수를 고려합니다.
-
-            // N을 i번 사용하여 숫자를 만듭니다.
-            // StringBuilder를 사용하여 N을 i번 반복하여 숫자를 생성합니다.
+            // make number with N, i times
             StringBuilder sb = new StringBuilder().append(N);
             for (int j = 1; j < i; j++) {
                 sb.append(N);
             }
             dp.get(i).add(Integer.parseInt(sb.toString()));
 
-            // 숫자를 더하거나 빼는 연산을 적용하여 가능한 숫자들을 생성합니다.
-            // dp 리스트를 이용하여 가능한 숫자들을 구합니다.
-            // dp[j]와 dp[i-j]에 저장된 숫자들을 이용하여 i에 해당하는 숫자를 만듭니다.
-            // 연산 결과를 dp[i]에 추가합니다.
+            // calculate using dp[j] & dp[i-j]
             for (int j = 1; j < i; j++) {
                 int k = i - j;
                 for (int num1 : dp.get(j)) {
@@ -45,21 +39,12 @@ class Solution {
                 }
             }
 
-            // number가 가능한 숫자들 중에 포함되는지 확인합니다.
-            // 만약 number가 포함되어 있다면 i를 반환합니다.
+            // check set includes number
             if (dp.get(i).contains(number)) {
                 return i;
             }
         }
 
-        // number를 표현할 수 없는 경우
         return -1;
-    }
-}
-
-public class Main(){
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.solution(5, 12);
     }
 }
