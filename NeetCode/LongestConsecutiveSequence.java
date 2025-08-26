71737c2311bdd289de7c44da524f35232045b49f
jerry8534 <jerryjjinu@gmail.com>
2025-08-26 10:04:42 +0900

    NeetCode Arrays & Hashing

---
+++ NeetCode/LongestConsecutiveSequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums == null || nums.length == 0) return new ;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        List<Integer> sortedSet = new ArrayList<>(set);
+        Collections.sort(sortedSet);
+        int max = 0;
+        int temp = sortedSet.get(0);
+        int tm = 1;
+        for(int num : sortedSet){
+            if(num - temp == 1) {
+                tm++;
+                temp = num;
+                if(tm > max){
+                    max = tm;
+                }
+            } else {
+                tm = 1;
+                temp = num;
+            }
+        }
+        return max;
+    }
+}
